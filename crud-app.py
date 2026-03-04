8ae9fe3e28a0633c3f30fa0c233a5060dd28f692
silviakun67-cyber <silviakun67@gmail.com>
2026-03-04 17:38:13 +0800

    Update crud-app.py

--- crud-app.py
+++ crud-app.py
@@ -19,7 +19,7 @@ gc = init_connection()
 # 2. 開啟指定的試算表與工作表
 # ==========================================
 SHEET_INPUT = "https://docs.google.com/spreadsheets/d/15SFMTq3tT2XRHY960diuxKYfFAQlbpj2QFSmjdbaIf8/edit?usp=sharing"
-WORKSHEET_NAME = "工作表1"
+WORKSHEET_NAME = "Sheet1"
 
 try:
     if SHEET_INPUT.startswith("http://") or SHEET_INPUT.startswith("https://"):
